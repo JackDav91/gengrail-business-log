1f114f554fbbb33d54eafeb1390f8971a7c21033
JackDav91 <166442005+JackDav91@users.noreply.github.com>
2026-08-09 22:50:41 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,34 +1,5 @@
-const C='gengrail-log-v8-persistence-test';
-const A=[
-  './',
-  './index.html',
-  './manifest.json',
-  './gengrail-theme.css',
-  './gengrail-ebay.js',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(C).then(c=>c.addAll(A)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch',e=>{
-  e.respondWith(
-    fetch(e.request)
-      .then(response=>{
-        const copy=response.clone();
-        caches.open(C).then(cache=>cache.put(e.request,copy));
-        return response;
-      })
-      .catch(()=>caches.match(e.request))
-  );
-});
+const C='gengrail-log-v10-command-centre';
+const A=['./','./index.html','./manifest.json','./gengrail-theme.css','./gengrail-ebay.js','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
